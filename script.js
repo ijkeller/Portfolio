a5b814d3a2638362336f86dea58b5256fdbccd6e
Isaiah Keller <isaiah.keller+git@gmail.com>
2022-01-28 09:03:37 -0800

    finished product, some post mvp items could be further

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-if (typeof $ == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file properly!')};
+// if (typeof $ == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+//   } else {console.log('I did it! I linked jQuery and this js file properly!')};
 
 
 
 $( "#hamburger").on("click", function() {
   console.log("hamburger click");
   $("#hb_links").toggleClass("show");
-})
+});
 
 
-const mediaQuery = window.matchMedia("(min-width: 768px)");
 
 const $projectsExtend = function() {
-  if (mediaQuery.matches) {
-    // Then trigger an alert
-  console.log("(min-width: 768px) + 'project' clicked");
-  $("#skills-list-media").toggleClass("skills_display");
-  $("#projects_images_media").toggleClass("projects_display");
-}}
+  console.log("'project' clicked");
+  $("#projects_images_media").toggleClass("projects_hide");
+};
 
 const $skillsExtend = function() {
-  if (mediaQuery.matches) {
-  // Then trigger an alert
-  console.log("(min-width: 768px) + 'skills' clicked");
-  $("#projects_images_media").toggleClass("projects_display");
-  $("#skills-list-media").toggleClass("skills_display");
-}}
+  console.log("'skills' clicked");
+  $("#skills-list-media").toggleClass("skills_hide");
+};
 
-$("#skillsTitle").on("click", $skillsExtend);
 
 $("#projectsTitle").on("click", $projectsExtend);
 
-
+$("#skillsTitle").on("click", $skillsExtend);
 
 
 
